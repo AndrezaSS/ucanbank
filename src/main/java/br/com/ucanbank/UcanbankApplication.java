3b928ec3907296fcab17606b61c10520dca5e2ef
Andreza Santos <andreza.ss1994@gmail.com>
2022-10-29 11:18:56 -0300

    Initial commit settings

---
+++ src/main/java/br/com/ucanbank/UcanbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.ucanbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcanbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcanbankApplication.class, args);
+	}
+
+}
