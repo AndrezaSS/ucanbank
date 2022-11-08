f7783daa6f62214ce536196e012b7c41590fae3a
Andreza Santos <andreza.ss1994@gmail.com>
2022-11-07 21:41:39 -0300

    Criação das classes Controller

---
+++ src/main/java/br/com/ucanbank/controller/ContaController.java
@@ -0,0 +1,13 @@
+package br.com.ucanbank.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conta")
+
+public class ContaController {
+
+}
